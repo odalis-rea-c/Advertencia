9e603dea206077e02d9175e468a5dd0088cb7a05
odalis-rea-c <osxyrea@gmail.com>
2025-04-02 11:44:09 -0500

    Advertencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogModalComponent } from './components/dialog-modal/dialog-modal.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto';
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    this.dialog.open(DialogModalComponent);
+  }
 }
---
+++ src/app/components/dialog-modal/dialog-modal.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-modal',
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './dialog-modal.component.html',
+  styleUrl: './dialog-modal.component.css'
+})
+export class DialogModalComponent {
+
+}
